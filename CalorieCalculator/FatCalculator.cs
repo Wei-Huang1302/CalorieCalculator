5b50b8a9a9e6980ac64686af62e193acba7c0ec5
Joe Huang <xhuang7970@conestogac.on.ca>
2019-01-29 22:12:41 -0500

    Add project files.

---
+++ CalorieCalculator/FatCalculator.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CalorieCalculator
+{
+    class FatCaculator : INotifyPropertyChanged
+    {
+        //create several constants that will be used in calculation
+        const double FAT_Convert_Unit = 9.0;
+        const double PERCENTAGE = 100.0;
+        const double HIGH_FAT_POINT = 30.0;
+        const string ZERO = "0";
+        private bool highOrLowFat = false;
+        public bool HighOrLowFat
+        {
+            get { return highOrLowFat; }
+            set { highOrLowFat = value; ShowCategoryResult(); NotifyChanged(); }
+        }
+        private string fatContentInput = ZERO;
+        public string FatContentInput
+        {
+            get { return fatContentInput; }
+            set
+            {
+                //Credited: Jaime Borges, StackOverflow
+                Regex regex = new Regex(@"^\d+(?:\.\d{0,3})?$");
+                if (regex.IsMatch(value))
+                {
+                    fatContentInput = value; CaloriesFromFat(); FatPercentageInFood(); ShowCategoryResult(); NotifyChanged();
+                }
+            }
+        }
+        private string totalCaloriesInput = ZERO;
+        public string TotalCaloriesInput
+        {
+            get { return totalCaloriesInput; }
+            set
+            {
+                //Credited: Jaime Borges, StackOverflow
+                Regex regex = new Regex(@"^\d+(?:\.\d{0,3})?$");
+                if (regex.IsMatch(value))
+                {
+                    totalCaloriesInput = value; FatPercentageInFood(); CaloriesFromFat(); ShowCategoryResult(); NotifyChanged();
+                }
+            }
+        }
+        private string fatPercentage = "";
+        public string FatPercentage
+        {
+            get { return fatPercentage; }
+            set { fatPercentage = value; NotifyChanged(); }
+        }
+        private string fatCalories = "";
+        public string FatCalories
+        {
+            get { return fatCalories; }
+            set { fatCalories = value; NotifyChanged(); }
+        }
+        private string categoryResult = "";
+        public string CategoryResult
+        {
+            get { return categoryResult; }
+            set { categoryResult = value; NotifyChanged(); }
+        }
+        public void CaloriesFromFat()
+        {
+            FatCalories = (double.Parse(FatContentInput) * FAT_Convert_Unit).ToString("0.00");
+        }
+        public void FatPercentageInFood()
+        {
+            FatPercentage = ((double.Parse(FatContentInput)) * FAT_Convert_Unit * PERCENTAGE / (double.Parse(TotalCaloriesInput))).ToString("0.00");
+        }
+        public void ShowCategoryResult()
+        {
+            if (HighOrLowFat == true)
+            {
+                if (((double.Parse(FatContentInput)) * FAT_Convert_Unit * PERCENTAGE / (double.Parse(TotalCaloriesInput))) >= HIGH_FAT_POINT)
+                {
+                    CategoryResult = "High Fat Food";
+                }
+                else
+                {
+                    CategoryResult = "Low Fat Food";
+                }
+            }
+            else
+            {
+                CategoryResult = "";
+            }
+        }
+        public void ResetButton()
+        {
+            CategoryResult = "";
+            FatContentInput = ZERO;
+            TotalCaloriesInput = ZERO;
+            HighOrLowFat = false;
+        }
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion                                    
+    }
+
+}
---
+++ CalorieCalculator/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalorieCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FatCaculator fc = new FatCaculator();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = fc;
+        }
+        //Reset everthing
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            fc.ResetButton();
+        }
+    }
+}
